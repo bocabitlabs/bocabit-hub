aadd3c27fce95610a6e6115d3f3d5ee07ede5082
René Fernández <renefs87@gmail.com>
2020-05-15 20:42:53 +0200

    Update react

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,35 @@ import logo from '../logo2.png'
 import ExternalLink from './ExternalLink'
 
 class Home extends Component {
-  blogUrl = 'http://blog.bocabit.com'
-  personalSiteUrl = 'http://renefernandez.com'
+  blogUrl = 'https://blog.bocabit.com'
+  personalSiteUrl = 'https://renefernandez.com'
 
-  render () {
+  render() {
     ReactGA.pageview('/')
     return (
       <div className={'Aligner-item'}>
         <Grid stackable columns={1} centered padded>
           <Grid.Column>
             <Segment.Group>
+              <Header as='h2' icon textAlign='center'>
+                <Image
+                  centered
+                  size='large'
+                  src='logo'
+                />
+                <Header.Content>{'bocabit.com'}</Header.Content>
+              </Header>
+
               <Segment textAlign={'center'} className={'main-links'}>
                 <Header as={'h1'}>
-                  <Image src={logo} rounded /> {'bocabit.com'}
+                  <Image src={logo} rounded />
                 </Header>
               </Segment>
               <Segment textAlign={'center'} className={'main-links'}>
-                <ExternalLink linkUrl={this.blogUrl} linkLabel={'Blog'} />
+                <a linkUrl={this.blogUrl} title={'Blog'} >Blog</a>
               </Segment>
               <Segment textAlign={'center'} className={'main-links'}>
-                <ExternalLink linkUrl={this.personalSiteUrl} linkLabel={'Personal Site'} />
+                <a href={this.personalSiteUrl} title={'Personal Site'}></a>
               </Segment>
               <Segment basic textAlign={'center'}>
                 <small>{'2020 - René Fernandez'}</small>
